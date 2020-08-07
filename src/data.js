79812ddb90fc778bffc8c331c937efae79223697
Nathiuk <nathaliasaray0220@gmail.com>
2020-08-06 20:05:08 -0500

    pushing the original updates from master

--- src/data.js
+++ src/data.js
@@ -1,9 +1,38 @@
+// // estas funciones son de ejemplo
+
+// export const example = () => {
+//   return 'example';
+// };
+
+// export const anotherExample = () => {
+//   return 'OMG';
+// };
+
+// FUNCIONES
 // estas funciones son de ejemplo
 
-export const data = () => {
-  return 'example';
-};
+// export const filter = () => {
 
-export const anotherExample = () => {
-  return 'OMG';
+//   Array.prototype.filter()
+
+//   return 'example';
+// };
+
+// export const anotherExample = () => {
+//   return 'OMG';
+// };
+
+// si comento este codigo, el boto de la 2da ventana no funciona
+export const orderSort = (setData, order) => {
+	let sorting = setData.sort(function (a, z) {
+		if (a.name > z.name) {
+			return 1;
+		} else {
+			return -1;
+		}
+	});
+	if (order === "Z-A") {
+		sorting.reverse();
+	}
+	return sorting;
 };
--- src/main.js
+++ src/main.js
@@ -1,18 +1,51 @@
-// import {  } from './data.js';
-// import data from './data/lol/lol.js';
+// import { example } from './data.js';
+// // import data from './data/lol/lol.js';
+// import data from './data/pokemon/pokemon.js';
+// // import data from './data/rickandmorty/rickandmorty.js';
+
+// console.log(example, data);
+
+import { orderSort } from "./data.js";
+
+// EVENTOS
+
+// import { example } from './data.js';
+
 import data from "./data/pokemon/pokemon.js";
-// import data from './data/rickandmorty/rickandmorty.js';
 
 // -----Dar función al botón para cambiar de vista----
 
 var next = document.getElementById("continue");
 
-next.addEventListener("click", nextPage);
+next.addEventListener("click", () => {
+	document.getElementById("Home").style.display = "none";
+	document.getElementById("Pokedex").style.display = "block";
+});
 
-function nextPage() {
-  document.getElementById("window-1").style.display = "none";
-  document.getElementById("window-2").style.display = "block";
-}
+// ----- Menú y vistas ----
+// const tabs = Array.prototype.slice.apply(document.querySelectorAll("data-tab-target"));
+// const window = Array.prototype.slice.apply(document.querySelectorAll("[data-tab-content]"));
+// document.getElementById("tabs").addEventListener("click", e => {
+//     if (e.target.classList.contains("data-tab-target")) {
+//         let i = tabs.indexOf(e.target);
+//         window.map(window => window.classList.remove("window-1"));
+//         window[i].classList.add("block");
+//     }
+// })
+
+var home = document.getElementById("home");
+
+home.addEventListener("click", () => {
+	document.getElementById("Pokedex").style.display = "none";
+	document.getElementById("Home").style.display = "grid";
+});
+
+var pokedex = document.getElementById("pokedex");
+
+pokedex.addEventListener("click", () => {
+	document.getElementById("Home").style.display = "none";
+	document.getElementById("Pokedex").style.display = "block";
+});
 
 // -----Desglosando el objeto de la data----
 // var info = dataSet[0];
@@ -22,11 +55,53 @@ function nextPage() {
 // var sum = (`${id} ${num} ${name}`);
 
 // -----Mostrar los pokemones en el HTML
-var dataSet = data.pokemon;
+let dataSet = data.pokemon;
 let result;
 for (result of dataSet) {
-  document.getElementById(
-    "window-container"
-  ).innerHTML += `<div class="card"><h2> #${result.num} </h2><p> ${result.name} </p><br><img src=${result.img}><div>`;
+	document.getElementById(
+		"window-container"
+	).innerHTML += `<div class="card"><h2> ${result.num} </h2><p> ${result.name} </p><br><img src=${result.img}><div>`;
 }
 
+// -----Botón de Ordenar----
+// let orderBtn = document.getElementById("aZ");
+// orderBtn.addEventListener("change", () => {
+//     var orderOption = orderBtn.options[orderBtn.selectedIndex].text;
+//     console.log(orderOption)
+// })
+
+let orderBtn = document.getElementById("aZ");
+orderBtn.addEventListener("change", (event) => {
+	var arrData = orderSort(dataSet, event.target.value);
+	// console.log(arrData);
+	var orderOption = document.querySelector(".window-container");
+	orderOption.textContent = "";
+	for (let pokemon of arrData) {
+		// console.log(pokemon);
+		orderOption.innerHTML += `<div class="card"><h2> ${pokemon.num} </h2><p> ${pokemon.name} </p><br><img src=${pokemon.img}><div>`;
+	}
+});
+
+// -----Botón de Filtro por Tipos----
+
+let typeBtn = document.getElementById("element");
+typeBtn.addEventListener("change", (event) => {
+	var typeOption = document.querySelector(".window-container");
+	// console.log(typeOption);
+	typeOption.textContent = event.target.value;
+});
+
+// let typeBtn = document.getElementById("element")
+// typeBtn.addEventListener("change", () => {
+//     var typeOption = typeBtn.options[typeBtn.selectedIndex].text;
+//
+// })
+
+// aZ.addEventListener("change", orderBtn);
+
+// function orderBtn () {
+//     var az= event.target.value;
+//     var za = prueba(dataSet, az);
+//       pintarpokemones(za);
+//       console.log(pintarpokemones)
+// }
